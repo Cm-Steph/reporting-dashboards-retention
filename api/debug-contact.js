b224780df358c41bd55b3a8f24089d00180ae91d
Cm-Steph <steph@clinicmastery.com>
2026-04-23 16:10:18 +1000

    Update debug-contact.js

--- api/debug-contact.js
+++ api/debug-contact.js
@@ -1,11 +1,9 @@
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  const apiKey     = process.env.GHL_API_KEY;
-  const locationId = process.env.GHL_LOCATION_ID;
-  const pipelineId = process.env.GHL_PIPELINE_ID;
+  const apiKey = process.env.GHL_API_KEY;
 
-  if (!apiKey || !locationId || !pipelineId) {
+  if (!apiKey) {
     return res.status(500).json({ error: 'not_configured' });
   }
 
@@ -16,29 +14,14 @@ module.exports = async function handler(req, res) {
   };
 
   try {
-    // Search for Joe Keain specifically
-    const searchRes = await fetch(
-      `https://services.leadconnectorhq.com/contacts/?locationId=${locationId}&query=Joe+Keain&limit=5`,
-      { headers }
-    );
-    const searchData = await searchRes.json();
-    const contacts = searchData.contacts || [];
-    const joe = contacts[0];
-
-    if (!joe) {
-      return res.status(200).json({ error: 'Joe Keain not found', searchData });
-    }
-
-    // Fetch full contact
     const contactRes = await fetch(
-      `https://services.leadconnectorhq.com/contacts/${joe.id}`,
+      'https://services.leadconnectorhq.com/contacts/GgxGiNCnh6YlGHon8T5l',
       { headers }
     );
     const contactData = await contactRes.json();
     const contact = contactData.contact || contactData;
 
     return res.status(200).json({
-      contactId: joe.id,
       contactName: contact.firstName + ' ' + contact.lastName,
       customFields: contact.customFields || []
     });
